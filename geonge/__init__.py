7395207b904444c2cec10e396e31e26adae1ca87
Deng Shaojiang <dengshaojiang@kingsoft.com>
2016-11-14 12:05:37 +0800

    Add requirments.txt file and declare on source header

---
+++ geonge/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (C) 2016,  All Rights Reserved.
+# Written by Deng Shaojiang (forshaojiang@gmail.com)
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public Licence# as published by the Free Software Foundation; either version
+# 2 of the Licence, or (at your option) any later version.
+
